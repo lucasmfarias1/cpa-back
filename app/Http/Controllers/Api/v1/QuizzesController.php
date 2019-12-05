47a9d3a6823f2e982619234680f776b4bb42263c
vagrant <vagrant@homestead>
2019-12-05 02:00:06 +0000

    initial commit

---
+++ app/Http/Controllers/Api/v1/QuizzesController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Quiz;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class QuizzesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $quizzes = Quiz::all();
+
+        return response()->json(['quizzes' => $quizzes], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $quiz = Quiz::create($request->all());
+
+        return response()->json(['quiz' => $quiz], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quiz $quiz)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Quiz $quiz)
+    {
+        $quiz->update($request->all());
+
+        return response()->json(['quiz', $quiz], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quiz $quiz)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_12_05_013609_create_quizzes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name');
+            $table->boolean('active');
+            $table->date('deadline');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
