126a9b9143c07336a2cab9ae68ac0c9f156eb907
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2020-01-08 22:58:52 -0300

    inserir contagem de questoes do quiz em seu model

--- app/Quiz.php
+++ app/Quiz.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     protected $fillable = ['name', 'deadline', 'active'];
+    protected $appends = ['question_count'];
 
     public function questions()
     {
         return $this->hasMany(Question::class);
+    }
+
+    public function getQuestionCountAttribute()
+    {
+        return $this->questions->count();
     }
 }
